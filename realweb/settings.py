233dd74b7f934d26b936ea2ca64f91698eccfd40
11056050 <11056050@ntub.edu.tw>
2023-09-27 15:48:17 +0800

    second

--- realweb/settings.py
+++ realweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--oun4iy_*74vntlbo$()vv7dr^*4te^05enmh$4h3v3m9xc#3_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
